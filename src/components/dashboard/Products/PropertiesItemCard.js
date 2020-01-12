d58ae61b06f27425295314f040a810969ccbb2b5
Bruno Sholla <brunosholla@gmail.com>
2020-01-12 18:48:46 +0100

    Added Products

--- src/components/dashboard/Products/PropertiesItemCard.js
+++ src/components/dashboard/Products/PropertiesItemCard.js
@@ -1,29 +1,27 @@
-import React ,{Component} from "react";
-import {Card, Col, Row,Button,Divider,Icon} from "antd";
+import React, {Component} from "react";
+import {Card, Col, Row, Button, Divider, Icon} from "antd";
 import ReactToPrint from "react-to-print";
 import {printDiv} from "../../../util/customFunctions";
 
 const {Meta} = Card;
 
-class PropertiesItemCard extends Component{
+class PropertiesItemCard extends Component {
 
     constructor(props) {
         super(props)
     }
 
-    render(){
-      //  console.log(this.props.data)
-    const {image, description, category, name, objectID, isFeatured, prize, sqft, bedrooms, baths, area, more, item, icons} = this.props.data;
-    return (
+    render() {
+        //  console.log(this.props.data)
+        const {image, description, category, name, objectID, isFeatured, prize, sqft, bedrooms, baths, area, more, item, icons} = this.props.data;
+        return (
 
-            <Col xl={8} lg={12} md={12} sm={24} xs={24} id={objectID}>
+            <Col xl={8} lg={12} md={12} sm={24} xs={24} onClick={(e) => printDiv(objectID)}>
                 <Card hoverable
                       cover={<img alt={description} src={image}/>}
-
-
                 >
                     <Meta
-                        avatar={<Button type="primary" shape="circle-outline" onClick={(e)=>printDiv(objectID)}>Ok</Button>}
+                        id={objectID}
                         title={name}
                         description={description}
                     />
@@ -32,8 +30,8 @@ class PropertiesItemCard extends Component{
             </Col>
 
 
-    );
-}
+        );
+    }
 }
 
-export default  PropertiesItemCard;
+export default PropertiesItemCard;
--- src/util/customFunctions.js
+++ src/util/customFunctions.js
@@ -1,7 +1,37 @@
+/*
 export function printDiv(divName) {
-    let w=window.open();
+    let w = window.open();
     var printContents = document.getElementById(divName).innerHTML;
     w.document.write(printContents);
     w.print();
     w.close();
+}*/
+
+export function printDisv(divName) {
+    let printContents = document.getElementById(divName).innerHTML;
+    let originalContents = document.body.innerHTML;
+
+    document.body.innerHTML = printContents;
+
+    window.print();
+
+    document.body.innerHTML = originalContents;
+    window.close();
+}
+
+export function printDiv(elem)
+{
+    let mywindow = window.open('', 'PRINT', 'height=400,width=600');
+
+    mywindow.document.write('<html><body >');
+    mywindow.document.write(document.getElementById(elem).innerHTML);
+    mywindow.document.write('</body></html>');
+
+    mywindow.document.close(); // necessary for IE >= 10
+    mywindow.focus(); // necessary for IE >= 10*/
+
+    mywindow.print();
+    mywindow.close();
+
+    return true;
 }
